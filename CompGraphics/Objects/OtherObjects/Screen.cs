5fbf5d39b540436d03d66ca29a1f0d2c66f52719
Andrew <svt.andrey1631@gmail.com>
2024-04-17 23:40:01 +0300

    filled screen class

--- CompGraphics/Objects/OtherObjects/Screen.cs
+++ CompGraphics/Objects/OtherObjects/Screen.cs
@@ -2,5 +2,31 @@
 
 public class Screen
 {
+    public char[,] Cells { get; private set; }
+    public int Width { get; }
+    public int Height { get; }
     
+    public Screen(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Cells = new char[height, width];
+    }
+    
+    public void SetCell(int i, int j, char ch)
+    {
+        Cells[i, j] = ch;
+    }
+    
+    public void Draw()
+    {
+        for (var i = 0; i < Height; i++)
+        {
+            for (var j = 0; j < Width; j++)
+            {
+                Console.Write(Cells[i, j] + " ");
+            } 
+            Console.WriteLine();
+        }
+    }
 }
